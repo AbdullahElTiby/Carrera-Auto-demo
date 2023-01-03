a9d87c744c6169d64e7269f5d48fa76f0a7cc8e6
Mahadi Hassan <itsmahadi@gmail.com>
2023-01-04 01:06:32 +0600

    updated

--- app/models.py
+++ app/models.py
@@ -5,9 +5,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class Usercus(AbstractUser):
     # Add additional fields here
-    username = models.TextField(null=True, blank=True, unique=True)
-    password = models.TextField(null=True, blank=True)
-    email = models.TextField(null=True, blank=True)
+    # username, password, email, first_name, last_name those are already there you dont need them to re declare
+    # username = models.TextField(null=True, blank=True, unique=True)
+    # password = models.TextField(null=True, blank=True)
+    # email = models.TextField(null=True, blank=True)
     phonenumber = models.TextField(null=True, blank=True)
